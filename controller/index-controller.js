e5e6308e31d96f29214072229a3b87bd076de1e4
Alain Buri <117375564+aburi-ost@users.noreply.github.com>
2023-06-16 15:43:16 +0000

    Add Ascending Sorting for Name, Due Date and Importance in index Add Filter Completed functionality in Index Add functionality to add completed tasks in ewEntryForm Add separate controller for noteEntry Add gray out for completed tasks

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -1,36 +1,81 @@
-import {noteEntryStore} from '../services/noteEntry-store.js';
+import { noteEntryStore } from "../services/noteEntry-store.js";
 
 export class IndexController {
+  DataBaseEntries = [];
+  FilteredDataBaseEntries = [];
 
-    index = async (req, res) => { // Defining an asynchronous function to show an order
-        res.render("index", {data: await noteEntryStore.all(), dark: true});
-    };
+  filterCompleted = () => {
+    this.FilteredDataBaseEntries = this.DataBaseEntries.filter(
+      (entry) => entry.state != "COMPLETED"
+    );
+  };
 
-    newNoteEntry = (req, res) => {
-        res.render("newEntryForm"); // Rendering the 'newOrder.hbs' file
-    };
+  sortByTitle = () => {
+    this.FilteredDataBaseEntries = this.DataBaseEntries.sort((a, b) => {
+      const nameA = a.title.toUpperCase();
+      const nameB = b.title.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  };
 
-    createNoteEntry = async (req, res) => { // Defining an asynchronous function to create a pizza
-        // Todo: please check this for validity. I Had to make the noteEntryStore.add function NOT async to be able to wait for it's conclusion to make sure the await noteEntryStore.all() reads the latest data from the database
+  sortByImportance = () => {
+    this.FilteredDataBaseEntries = this.DataBaseEntries.sort((a, b) => {
+      const nameA = a.importance;
+      const nameB = b.importance;
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  };
 
-        await noteEntryStore.add(
-            req.body.noteDueDate,
-            req.body.noteTitle,
-            req.body.noteImportance,
-            req.body.noteState,
-            req.body.noteDescription
-        );
+  sortByDueDate = () => {
+    this.FilteredDataBaseEntries = this.DataBaseEntries.sort((a, b) => {
+      const nameA = a.dueDate;
+      const nameB = b.dueDate;
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0; // a and b are equal in terms of sorting
+    });
+  };
 
-        res.render("index", {data: await noteEntryStore.all(), dark: true});
+  index = async (req, res) => {
+    if (req.query.fetchData === "true") {
+      // If fetchData is not included or set to any value other than 'true', the noteEntryStore.all() method will not be executed and data will remain an empty array.
+      this.DataBaseEntries = await noteEntryStore.all();
+    }
 
-    };
-    deleteOrder =async (req, res) => { // Defining an asynchronous function to delete an order
-        // Todo: Same here as in createNoteEntry. had to make noteEntryStore.delete synchronous
-        // Todo: include and use in delete functionality
-        await noteEntryStore.delete(req.params.noteId);
-        res.render("index", {data: await noteEntryStore.all(), dark: true});
-    };
+    this.FilteredDataBaseEntries = this.DataBaseEntries;
 
+    // Todo: manage with cookies instead of query. See: utils/session-middleware.index.js
+    if (req.query.filterCompleted === "true") {
+      this.filterCompleted();
+    }
+
+    // Todo: manage with cookies instead of query. See: utils/session-middleware.index.js
+    if (req.query.sortDataBy === "title") {
+      this.sortByTitle();
+    } else if (req.query.sortDataBy === "importance") {
+      this.sortByImportance();
+    } else if (req.query.sortDataBy === "dueDate") {
+      this.sortByDueDate();
+    }
+
+    res.render("index", { data: this.FilteredDataBaseEntries, dark: false });
+  };
 }
 
 export const indexController = new IndexController();
