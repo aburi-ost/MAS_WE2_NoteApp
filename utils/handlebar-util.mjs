7687ab26d1a3252a4a15fcd14fd37a122801aa24
Alain Buri <117375564+aburi-ost@users.noreply.github.com>
2023-07-12 12:22:27 +0000

    Add days representation of dueDate

--- utils/handlebar-util.mjs
+++ utils/handlebar-util.mjs
@@ -7,4 +7,23 @@ export const helpers = {
     get_EntryState_Completed: function () {
         return EntryState.Completed
     },
+    dueDate_in_Days: function (dueDate) {
+        const currentDate = new Date().setUTCHours(0, 0, 0, 0)
+        const inputDate = new Date(dueDate).setUTCHours(0, 0, 0, 0)
+        const daysUntilDate = (inputDate - currentDate) / (1000 * 60 * 60 * 24)
+        switch (true) {
+            case daysUntilDate === 0:
+                return 'now'
+            case daysUntilDate === -1:
+                return 'a day ago'
+            case daysUntilDate === 1:
+                return 'in a day'
+            case daysUntilDate > 1:
+                return `in ${daysUntilDate} days`
+            case daysUntilDate < 1:
+                return `${Math.abs(daysUntilDate)} days ago`
+            default:
+                return 'Someday'
+        }
+    },
 }
