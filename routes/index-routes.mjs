812fdf51673d6e87dc62ad4c1022372610cb170b
aburi-ost <alain.buri@ost.ch>
2023-07-13 22:27:08 +0200

    Add redirect route for POST requests to index-routes.mjs

--- routes/index-routes.mjs
+++ routes/index-routes.mjs
@@ -6,5 +6,6 @@ import { indexController } from '../controller/index-controller.mjs'
 // JavaScript kann "this" verloren gehen und mit bind instanz von indexController wird garantiert auf this verbunden. --> erlaubt sichere verwendung von this innerhalb der Klasse. ist aber im projekt nicht nötig
 indexRouter.get('/', indexController.index.bind(indexController))
 indexRouter.get('/index', indexController.index)
+indexRouter.post('/index', indexController.reorderIndex)
 
 export const indexRoutes = indexRouter
